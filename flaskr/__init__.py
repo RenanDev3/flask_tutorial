80d046b3c0b69ed1936960d2d2379f23e256be70
Renan Boaventura <renandev@outlook.com>
2023-07-15 17:42:19 -0300

    feature(flaskr): implement the init python file with the create app function

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,12 +6,17 @@ from flask import Flask
 
 
 def create_app(test_config=None):
+    # __name__ is the name of the current Python module.
+    # instance_relative_config=True tells the app that configuration files are relative to the instance folder
     app = Flask(__name__, instance_relative_config=True)
+
+    # app.config.from_mapping() sets some default configuration that the app will use
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
+    # you can use a config.py file or create a dict to instantiate create_app
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
@@ -20,7 +25,7 @@ def create_app(test_config=None):
     try:
         os.makedirs(app.instance_path)
     except Exception as e:
-        raise e
+        pass
 
     @app.route('/hello')
     def hello():
